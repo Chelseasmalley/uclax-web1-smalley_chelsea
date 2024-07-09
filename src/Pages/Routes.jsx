015f1bc5820ea9538c063f28ab9990f3d4884deb
Chelsea Smalley <chelseasmalley97@gmail.com>
2024-07-08 18:42:10 -0700

    L3 Complete

---
+++ src/Pages/Routes.jsx
@@ -0,0 +1,42 @@
+import { Routes, Route, HashRouter } from "react-router-dom";
+// Global Styles
+import "@/Common/css/normalize.css";
+import "@/Common/css/global.scss";
+
+// layout
+import PagesLayout from "@/Common/PagesLayout/PagesLayout.jsx";
+
+// page components
+import Home from "./Home";
+import Staff from "./Staff";
+import Contact from "./Contact";
+
+// course work page components
+import CourseWorkLayout from "./Coursework/CourseWorkLayout.jsx";
+import SunAndMoon from "./Coursework/SunAndMoon/SunAndMoon.jsx";
+import Essays from "./Coursework/Essays";
+import ResponsiveDesign from "./Coursework/ResponsiveDesign";
+
+const MyRoutes = () => {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route element={<PagesLayout />}>
+                    <Route element={<Home />} path="" />
+                    <Route element={<Staff />} path="staff" />
+                    <Route element={<Contact />} path="contact" />
+                    <Route element={<CourseWorkLayout />} path="course-work">
+                        <Route element={<Essays />} path="" />
+                        <Route element={<SunAndMoon />} path="sun-and-moon" />
+                        <Route
+                            element={<ResponsiveDesign />}
+                            path="responsive"
+                        />
+                    </Route>
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
+};
+
+export default MyRoutes;
