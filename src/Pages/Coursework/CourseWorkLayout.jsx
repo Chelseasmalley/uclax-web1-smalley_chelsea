9b7bb2da42b33555f067f8dc98f5c9c4ee9a5678
Chelsea Smalley <chelseasmalley97@gmail.com>
2024-07-21 23:05:52 -0700

    L05 Complete

--- src/Pages/Coursework/CourseWorkLayout.jsx
+++ src/Pages/Coursework/CourseWorkLayout.jsx
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 import { Outlet, NavLink } from "react-router-dom";
+import Inset from "@/Common/PagesLayout/Inset";
 
-const CourseWorkLayout = ({}) => {
+const CourseWorkLayout = () => {
     return (
         <CourseWorkLayoutStyled>
-            <h1>Course Work</h1>
-            <nav className="sublinks">
-                <NavLink to="/course-work">Essays</NavLink>
-                <NavLink to="/course-work/sun-and-moon">Sun And Moon </NavLink>
-                <NavLink to="/course-work/responsive">Responsive</NavLink>
-            </nav>
-            <Outlet />
+            <Inset>
+                <h1>Course Work</h1>
+                <nav className="sublinks">
+                    <NavLink to="/course-work">Essays</NavLink>
+                    <NavLink to="/course-work/sun-and-moon">
+                        Sun And Moon{" "}
+                    </NavLink>
+                    <NavLink to="/course-work/responsive">Responsive</NavLink>
+                </nav>
+                <Outlet />
+            </Inset>
         </CourseWorkLayoutStyled>
     );
 };
